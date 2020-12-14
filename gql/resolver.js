801b9f9bdd3387a6acd79d467a9bf61bbf1377d5
davidcrc <david_drcr@hotmail.com>
2020-12-14 16:27:09 -0500

    5-3. Guardando el usuario en la base de datos

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+
 const resolver = {
     Query : {
         // User
@@ -10,10 +12,44 @@ const resolver = {
     Mutation: {
         
         // Params: _, el body , los headers
-        register: (_, {input}, conext) => {
+        register: async (_, {input}, conext) => {
             
-            console.log("Registrando un usuario")
-            console.log(input)
+            // console.log("Registrando un usuario")
+            // console.log(input)
+            const newUser = input;
+            newUser.email = newUser.email.toLowerCase();
+            newUser.username = newUser.username.toLowerCase();
+
+            const { email, username, password } = newUser;
+
+            const foundEmail = await User.findOne({email})
+
+            // Consultar Si email y username ya existen en la BD
+            if(foundEmail){
+                console.log("Email ya Registrado")
+                throw new Error("El email ya esta en uso");
+            }
+
+            const foundUsername = await User.findOne({username})
+
+            if(foundUsername){
+                console.log("Usuario ya Registrado")
+                throw new Error("El usuario ya esta en uso");
+            }
+            
+            // Encriptar
+            // ...
+
+            try {
+                const user = new User(newUser)
+                user.save();
+
+                return user;
+            } catch (error) {
+                console.log(error)
+            }
+
+            console.log(newUser);
 
             return input;
         }
