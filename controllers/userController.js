37ecffe5293268113c2651d68cba0811f234469c
davidcrc <david_drcr@hotmail.com>
2020-12-14 16:40:08 -0500

    5-5. Refactorizando las funciones del resolver

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require("../models/user")
+const bcryptjs = require("bcryptjs")
+
+async function register(input){
+            
+    // console.log("Registrando un usuario")
+    // console.log(input)
+    const newUser = input;
+    newUser.email = newUser.email.toLowerCase();
+    newUser.username = newUser.username.toLowerCase();
+
+    const { email, username, password } = newUser;
+
+    const foundEmail = await User.findOne({email})
+
+    // Consultar Si email y username ya existen en la BD
+    if(foundEmail){
+        console.log("Email ya Registrado")
+        throw new Error("El email ya esta en uso");
+    }
+
+    const foundUsername = await User.findOne({username})
+
+    if(foundUsername){
+        console.log("Usuario ya Registrado")
+        throw new Error("El usuario ya esta en uso");
+    }
+    
+    // Encriptar
+    const salt = await bcryptjs.genSaltSync(10);
+    newUser.password = await bcryptjs.hash(password, salt);
+
+    try {
+        const user = new User(newUser)
+        user.save();
+
+        return user;
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(newUser);
+
+    return input;
+}
+
+async function getUser(){
+
+    console.log("Obtener usuario")
+    return null;
+}
+
+module.exports = {
+    register,
+    getUser
+}
--- gql/resolver.js
+++ gql/resolver.js
@@ -1,60 +1,14 @@
-const User = require("../models/user")
-const bcryptjs = require("bcryptjs")
+const userController = require("../controllers/userController");
 
 const resolver = {
     Query : {
         // User
-        getUser: () => {
-
-            console.log("Obtener usuario")
-            return null;
-        }
+        getUser: () => userController.getUser()
     },
     Mutation: {
         
         // Params: _, el body , los headers
-        register: async (_, {input}, conext) => {
-            
-            // console.log("Registrando un usuario")
-            // console.log(input)
-            const newUser = input;
-            newUser.email = newUser.email.toLowerCase();
-            newUser.username = newUser.username.toLowerCase();
-
-            const { email, username, password } = newUser;
-
-            const foundEmail = await User.findOne({email})
-
-            // Consultar Si email y username ya existen en la BD
-            if(foundEmail){
-                console.log("Email ya Registrado")
-                throw new Error("El email ya esta en uso");
-            }
-
-            const foundUsername = await User.findOne({username})
-
-            if(foundUsername){
-                console.log("Usuario ya Registrado")
-                throw new Error("El usuario ya esta en uso");
-            }
-            
-            // Encriptar
-            const salt = await bcryptjs.genSaltSync(10);
-            newUser.password = await bcryptjs.hash(password, salt);
-
-            try {
-                const user = new User(newUser)
-                user.save();
-
-                return user;
-            } catch (error) {
-                console.log(error)
-            }
-
-            console.log(newUser);
-
-            return input;
-        }
+        register: async (_, {input}, conext) => userController.register(input)
     }
 }
 
